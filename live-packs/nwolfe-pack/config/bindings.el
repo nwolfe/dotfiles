28939a76b4fc075be55520f06ee43830691313b9
Nate Wolfe <nwolfe@puppetlabs.com>
2014-10-01 13:00:13 -0700

    Emacs smarter-move-to-beginning-of-line function

--- live-packs/nwolfe-pack/config/bindings.el
+++ live-packs/nwolfe-pack/config/bindings.el
@@ -3,3 +3,7 @@
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; Remap C-a to smarter-move-beginning-of-line
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-to-beginning-of-line)
