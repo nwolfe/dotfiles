8c4a7e7b556f039a352b86d26cdae195cfdd3b19
Nate Wolfe <nwolfe@puppetlabs.com>
2015-10-02 15:52:58 -0700

    Update profiles.clj dependencies

--- misc/profiles.clj
+++ misc/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
                   [lein-pprint "1.1.2"]
                   [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                       [org.clojure/clojure "1.7.0"]]}}
