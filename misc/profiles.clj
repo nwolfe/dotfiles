c2fc4b78de98f7da4ff8e06205fcdc0de29622c0
Nate Wolfe <nwolfe@puppetlabs.com>
2015-11-05 15:48:32 -0800

    Bump tools.nrepl to 0.2.12 in lein .profiles

--- misc/profiles.clj
+++ misc/profiles.clj
@@ -2,5 +2,5 @@
                   [lein-pprint "1.1.2"]
                   [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/clojure "1.7.0"]]}}
