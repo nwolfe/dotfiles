80cb73933abc9e9173ebad79b4feaaee47769b24
Nate Wolfe <nwolfe@puppetlabs.com>
2015-12-15 14:39:33 -0800

    Upgrade cider-nrepl dependency in profiles.clj

--- misc/profiles.clj
+++ misc/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
                   [lein-pprint "1.1.2"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/clojure "1.7.0"]]}}
