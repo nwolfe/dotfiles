07d2edf7cf2c38461362402f862e9b09b462cdf9
Nate Wolfe <nwolfe@puppetlabs.com>
2016-01-11 12:03:35 -0800

    Remove lein-difftest from profiles.clj

--- misc/profiles.clj
+++ misc/profiles.clj
@@ -1,5 +1,4 @@
-{:user {:plugins [[lein-difftest "2.0.0"]
-                  [lein-pprint "1.1.2"]
+{:user {:plugins [[lein-pprint "1.1.2"]
                   [cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
