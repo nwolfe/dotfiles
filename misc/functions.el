1f91ab947f271f64ba42f5ee853f4bbebcf16dbf
Nate Wolfe <nwolfe@puppetlabs.com>
2016-03-30 14:28:39 -0700

    [functions] Delete WIP r10k functions

--- misc/functions.el
+++ misc/functions.el
@@ -5,21 +5,11 @@
            "source local/bin/activate && "
            "jenkins-jobs --conf enterprise/builder.conf test "
            "resources/:enterprise/projects "
-           "\"experimental_" arg "_*\"")))
+           "\"" arg "_*\"")))
 
 (defun ncw/jjb-test-prompt (arg)
   (interactive "sName regex: ")
   (ncw/jjb-do-test arg))
-
-(defun ncw/jjb-test-WIP-pe-r10k-vanagon ()
-  (interactive)
-  (ncw/jjb-do-test "WIP-pe-r10k-vanagon"))
-
-(defun ncw/jjb-test-WIP-r10k ()
-  (interactive)
-  (ncw/jjb-do-test "WIP-r10k"))
-
-
 
 (defun ncw/jjb-do-deploy (arg)
   (async-shell-command
@@ -30,11 +20,3 @@
 (defun ncw/jjb-deploy-prompt (arg)
   (interactive "sName regex: ")
   (ncw/jjb-do-deploy arg))
-
-(defun ncw/jjb-deploy-WIP-pe-r10k-vanagon ()
-  (interactive)
-  (ncw/jjb-do-deploy "WIP-pe-r10k-vanagon"))
-
-(defun ncw/jjb-deploy-WIP-r10k ()
-  (interactive)
-  (ncw/jjb-do-deploy "WIP-r10k"))
