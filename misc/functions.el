da0af42a79340f3f92996dc3e0e9d8a899f8a7f1
Nate Wolfe <nwolfe@puppetlabs.com>
2016-05-03 11:43:48 -0700

    [functions] Add jenkins instance parameter to jjb functions

--- misc/functions.el
+++ misc/functions.el
@@ -1,15 +1,15 @@
 
-(defun ncw/jjb-do-test (arg)
+(defun ncw/jjb-do-test (jenkins job)
   (async-shell-command
    (concat "cd $PUPPETLABS/ci-job-configs && "
            "source local/bin/activate && "
-           "jenkins-jobs --conf jenkii/enterprise/builder.conf test "
-           "resources/:jenkii/enterprise/projects "
-           "\"" arg "_*\"")))
+           "jenkins-jobs --conf jenkii/" jenkins "/builder.conf test "
+           "resources/:jenkii/" jenkins "/projects "
+           "\"" job "_*\"")))
 
-(defun ncw/jjb-test-prompt (arg)
-  (interactive "sName regex: ")
-  (ncw/jjb-do-test arg))
+(defun ncw/jjb-test-prompt (jenkins job)
+  (interactive "sJenkins: \nsName regex: ")
+  (ncw/jjb-do-test jenkins job))
 
 (defun ncw/jjb-do-deploy (arg)
   (async-shell-command
