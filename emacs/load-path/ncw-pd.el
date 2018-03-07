d1f33e6a5e6d87aaeda74d9761384a58b6dc0a5a
Nate Wolfe <nwolfe@puppet.com>
2018-03-06 17:52:27 -0800

    Update PD utilities

--- emacs/load-path/ncw-pd.el
+++ emacs/load-path/ncw-pd.el
@@ -1,20 +1,16 @@
-;; ;; A copy of "make bazel-test" but skip PDP tests
-;; (defvar cmd-test-nopdp
-;;   (concat
-;;    "bazel test --test_verbose_timeout_warnings $("
-;;    "bazel query 'tests(//... - //test/journey/... - //workstation/... - //pdp/...)'"
-;;    " | xargs)"))
-
-(defvar cmd-test "make bazel-test")
-
-(defvar cmd-buildcli "make -C workstation build")
-
 (defun ncw/pd-test ()
   (interactive)
-  (async-shell-command (concat "cd $PUPPETLABS/cloud-discovery && " cmd-test)))
+  (async-shell-command (concat "cd $PUPPETLABS/cloud-discovery && "
+                               "make test")))
+
+(defun ncw/pd-test-cli ()
+  (interactive)
+  (async-shell-command (concat "cd $PUPPETLABS/cloud-discovery && "
+                               "make -C workstation unit-tests")))
 
 (defun ncw/pd-buildcli ()
   (interactive)
-  (async-shell-command (concat "cd $PUPPETLABS/cloud-discovery && " cmd-buildcli)))
+  (async-shell-command (concat "cd $PUPPETLABS/cloud-discovery && "
+                               "make -C workstation build")))
 
 (provide 'ncw-pd)
