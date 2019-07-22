f4a03a80764d49e81c3099721d6bffa1afd3d472
Nate Wolfe <nwolfe@puppet.com>
2019-07-22 15:06:06 -0700

    [doom-emacs] Update config

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -63,6 +63,7 @@
 
        :term
        ;;eshell              ; a consistent, cross-platform shell (WIP)
+       ;;shell               ; a terminal REPL for Emacs
        term                  ; terminals in Emacs
        ;;vterm               ; another terminals in Emacs
 
