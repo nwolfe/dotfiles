3a77d8cd96955e9403e117f41202d804052c1d59
Nate Wolfe <nwolfe@puppet.com>
2019-08-29 13:33:14 -0700

    [utils] Fix shellcheck warnings

--- utils/line.sh
+++ utils/line.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 # Prints a horizonatal line the full width of the terminal.
 # Optional argument $1 for the character to print; defaults to =.
 line() {
     char=${1:-=}
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" ${char} | tr ' ' ${char}
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" "${char}" | tr ' ' "${char}"
 }
--- utils/log.sh
+++ utils/log.sh
@@ -1,15 +1,18 @@
+#!/usr/bin/env bash
+
 # Use in place of `tee` to write to a logfile under $DOTFILES/.local/logs.
 
-source $DOTFILES/utils/line.sh
+# shellcheck source=/Users/nwolfe/.dotfiles/utils/line.sh
+source "$DOTFILES/utils/line.sh"
 
 log() {
     _logdir=$DOTFILES/.local/logs
-    [ ! -d $_logdir ] && mkdir -p $_logdir
-    _logfile=$_logdir/$(basename $1)
+    [ ! -d "$_logdir" ] && mkdir -p "$_logdir"
+    _logfile=$_logdir/$(basename "$1")
     # Use "+%Y-%m-%d-%H%M%S" for filename-suitable timestamp
     _timestamp=$(date)
-    echo $(line =) > $_logfile
-    echo "Timestamp:" $_timestamp >> $_logfile
-    echo $(line -) >> $_logfile
-    tee -a $_logfile
+    line = > "$_logfile"
+    echo "Timestamp:" "$_timestamp" >> "$_logfile"
+    line - >> "$_logfile"
+    tee -a "$_logfile"
 }
