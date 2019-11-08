6ca1d86979a0844ff5582f1c53cf86ad2c9b57e2
Nate Wolfe <nwolfe@puppet.com>
2019-11-08 13:05:57 -0800

    [doom-emacs] Auto-enable +org-pretty-mode

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -1,5 +1,10 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+;; Auto-enable pretty mode for org files
+(add-hook! 'org-mode-hook '+org-pretty-mode)
+
+;; Shell out to `idea-open <file> <line>'
+;; to open the current file in IntelliJ IDEA
 (defun ncw/idea-open ()
   (interactive)
   (async-shell-command
