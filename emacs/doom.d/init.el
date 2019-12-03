8d2a8afd21be374540e1d3cbed68877cd0b3d1d3
Nate Wolfe <nwolfe@puppet.com>
2019-12-03 14:18:18 -0800

    [doom-emacs] Update config

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -112,7 +112,7 @@
        ;;faust               ; dsp, but you get to keep your soul
        ;;fsharp              ; ML stands for Microsoft's Language
        go                    ; the hipster dialect
-       ;;(haskell +intero)   ; a language that's lazier than I am
+       ;;(haskell +dante)    ; a language that's lazier than I am
        ;;hy                  ; readability of scheme w/ speed of python
        ;;idris
        ;;(java +meghanada)   ; the poster child for carpal tunnel syndrome
@@ -121,6 +121,7 @@
        ;;kotlin              ; a better, slicker Java(Script)
        ;;latex               ; writing papers in Emacs has never been so fun
        ;;lean
+       ;;factor
        ;;ledger              ; an accounting system in Emacs
        ;;lua                 ; one-based indices? one-based indices
        ;;markdown            ; writing docs for people to ignore
