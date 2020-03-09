1d96ac357e84c9484721eb2cf6be3ee357d07acf
Nate Wolfe <a1979shakedown@gmail.com>
2020-03-08 23:34:07 -0700

    Many updates
    * Install use-package and configure it
    * Configure custom-file so it doesn't edit init.el
    * Auto-update buffers when they change externally

--- init.el
+++ init.el
@@ -12,12 +12,23 @@
 (package-initialize)
 
 ;; Better defaults. No starter kits!
+;; TODO Get this as a package from MELPA instead?
 (add-to-list 'load-path "c:/Users/a1979/Documents/better-defaults")
 (require 'better-defaults)
+
+;; Get 'use-package' to install other packages
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+(setq use-package-always-ensure t)
 
 ;; Disable annoying autosave/backup files
 (setq auto-save-default nil)
 (setq make-backup-files nil)
+
+;; Store "custom" file separately
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
 ;; Enable indent mode for all .org files
 (setq org-startup-indented t)
@@ -27,3 +38,6 @@
 
 ;; No need for verbosity
 (fset 'yes-or-no-p 'y-or-n-p)
+
+;; Auto-update buffers when they are changed externally
+(global-auto-revert-mode t)
