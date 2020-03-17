cd0c92dc8a13500bd238d12fa37566494aec0b16
Nate Wolfe <nwolfe@puppet.com>
2020-03-17 11:25:29 -0700

    [doom-emacs] Swap Option<->Command keys

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -3,7 +3,12 @@
 ;; Auto-enable pretty mode for org files
 (add-hook! 'org-mode-hook '+org-pretty-mode)
 
+;; Default is 80 in Doom Emacs
 (setq-default fill-column 70)
+
+;; Swap Option<->Command keys so Meta key is easier to reach
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
 
 (defun ncw/idea-open ()
   "Shell out to `idea-open <file> <line>' to open current file in IntelliJ IDEA."
